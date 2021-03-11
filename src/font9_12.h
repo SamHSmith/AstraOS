c182892b0c9d9bf7f0e916f9564e7c61e2d810c5
Sam Smith <sam.henning.smith@protonmail.com>
2021-03-11 09:00:08 +0100

    symbols done

--- src/font9_12.h
+++ src/font9_12.h
@@ -48,15 +48,57 @@ void font9_12_get_bitmap(u64* bitmap)
         } else if(c == '=') {
             bitmap[2*c+0] = 0x7C000000000;
             bitmap[2*c+1] = 0x7C00;
-        } else if(c == '!') {
-            bitmap[2*c+0] = 0x4070381C0E07010;
+        } else if(c == '\'') {
+            bitmap[2*c+0] = 0x6060301800000;
+            bitmap[2*c+1] = 0x0;
+        } else if(c == '"') {
+            bitmap[2*c+0] = 0x6C361B0D800;
+            bitmap[2*c+1] = 0x0;
+        } else if(c == ',') {
+            bitmap[2*c+0] = 0x0;
+            bitmap[2*c+1] = 0x2020301800;
+        } else if(c == '.') {
+            bitmap[2*c+0] = 0x0;
+            bitmap[2*c+1] = 0x301800;
+        } else if(c == '<') {
+            bitmap[2*c+0] = 0x100808080800000;
+            bitmap[2*c+1] = 0x401004;
+        } else if(c == '>') {
+            bitmap[2*c+0] = 0x1008020080200000;
+            bitmap[2*c+1] = 0x101010;
+        } else if(c == '/') {
+            bitmap[2*c+0] = 0x202020101010000;
+            bitmap[2*c+1] = 0x8080404;
+        } else if(c == '?') {
+            bitmap[2*c+0] = 0x404040201107000;
             bitmap[2*c+1] = 0x40701000;
-        } else if(c == '!') {
-            bitmap[2*c+0] = 0x4070381C0E07010;
-            bitmap[2*c+1] = 0x40701000;
-        } else if(c == '!') {
-            bitmap[2*c+0] = 0x4070381C0E07010;
-            bitmap[2*c+1] = 0x40701000;
+        } else if(c == '{') {
+            bitmap[2*c+0] = 0x30181008040C000;
+            bitmap[2*c+1] = 0x180201008;
+        } else if(c == '}') {
+            bitmap[2*c+0] = 0x180C010080401800;
+            bitmap[2*c+1] = 0x30201008;
+        } else if(c == '\\') {
+            bitmap[2*c+0] = 0x802008040100400;
+            bitmap[2*c+1] = 0x200804010;
+        } else if(c == '|') {
+            bitmap[2*c+0] = 0x402010080402000;
+            bitmap[2*c+1] = 0x40201008;
+        } else if(c == ' ') {
+            bitmap[2*c+0] = 0x0;
+            bitmap[2*c+1] = 0x0;
+        } else if(c == '-') {
+            bitmap[2*c+0] = 0x3F80000000000000;
+            bitmap[2*c+1] = 0x0;
+        } else if(c == '_') {
+            bitmap[2*c+0] = 0x0;
+            bitmap[2*c+1] = 0x3F8000000;
+        } else if(c == ';') {
+            bitmap[2*c+0] = 0x180C0000000;
+            bitmap[2*c+1] = 0x6060300000;
+        } else if(c == ':') {
+            bitmap[2*c+0] = 0x180C0000000;
+            bitmap[2*c+1] = 0x60300000;
         }
     }
 }
