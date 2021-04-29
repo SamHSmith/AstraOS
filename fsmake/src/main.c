a3aaeefe7513c734fc949e4dd7308e42795dc1c2
Sam H Smith <sam.henning.smith@protonmail.com>
2021-04-29 04:49:59 +0200

    creating a harddrive and empty partition table

--- fsmake/src/main.c
+++ fsmake/src/main.c
@@ -6,7 +6,27 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <assert.h>
+#include <stdlib.h>
 
+#include <openssl/sha.h>
+
+typedef struct
+{
+    u8 uid[64];
+    u64 start_block;
+    u16 partition_type;
+    u8 name[54];
+} RAD_PartitionTableEntry;
+
+typedef struct
+{
+    u8 sha512sum[64];
+    u8 padding[64];
+    RAD_PartitionTableEntry entries[63];
+} RAD_PartitionTable;
+
+#define TABLE_COUNT 8
 
 void main(int argc, char** argv)
 {
@@ -19,6 +39,27 @@ void main(int argc, char** argv)
     for(u64 i = 0; i < 2048*32; i++)
     {
         write(drive_fd, block, 4096);
+    }
+
+    assert(sizeof(RAD_PartitionTable) == 4096*2);
+
+    RAD_PartitionTable* table = calloc(sizeof(RAD_PartitionTable));
+    assert(table != 0);
+
+    SHA512_CTX sha_ctx;
+
+    // fill out table
+
+
+    assert(SHA512_Init(&sha_ctx));
+    assert(SHA512_Update(&sha_ctx, ((u8*)table) + 64, sizeof(RAD_PartitionTable) - 64));
+    assert(SHA512_Final((u8*)table, &sha_ctx));
+
+    // write table
+    lseek(drive_fd, 0, SEEK_SET);
+    for(u64 i = 0; i < TABLE_COUNT; i++)
+    {
+        write(drive_fd, table, sizeof(*table));
     }
 
     close(drive_fd);
