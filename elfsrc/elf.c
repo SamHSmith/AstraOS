5f91f763ffc06e4ebc0b096ffec27ea3107f9a7b
Sam H Smith <sam.henning.smith@protonmail.com>
2021-06-10 12:04:25 +0000

    spinning square in elf

--- elfsrc/elf.c
+++ elfsrc/elf.c
@@ -31,40 +31,69 @@ while(1)
     u64 fb_page_count = AOS_surface_acquire(surface, 0, 0);
     if(AOS_surface_acquire(surface, fb, fb_page_count))
     {
-        f32 time = AOS_time_get_seconds() - start_time;
+        f64 frame_start = AOS_time_get_seconds();
+        f32 time = frame_start - start_time;
         f32 red = (sineF32((time*M_PI)/2.0) + 1.0) / 2.0;
         f32 green = (sineF32((time*M_PI)/3.0) + 1.0) / 2.0;
         f32 blue = (sineF32((time*M_PI)/5.0) + 1.0) / 2.0;
 
+        f32 s = cosineF32(time/10.0 * 2*M_PI);
+        f32 c = sineF32(time/10.0 * 2*M_PI);
+        f32 p1x = -0.25 * c -  0.25 * s;
+        f32 p1y =  0.25 * c + -0.25 * s;
+        f32 p2x =  0.25 * c -  0.25 * s;
+        f32 p2y =  0.25 * c +  0.25 * s;
+        f32 p3x =  0.25 * c - -0.25 * s;
+        f32 p3y = -0.25 * c +  0.25 * s;
+        f32 p4x = -0.25 * c - -0.25 * s;
+        f32 p4y = -0.25 * c + -0.25 * s;
+
+        f32 d1x = p2x - p1x;
+        f32 d1y = p2y - p1y;
+        f32 d2x = p3x - p2x;
+        f32 d2y = p3y - p2y;
+        f32 d3x = p4x - p3x;
+        f32 d3y = p4y - p3y;
+        f32 d4x = p1x - p4x;
+        f32 d4y = p1y - p4y;
+
+        f32 dpfx = 2.0 / (f32)fb->width;
+        f32 dpfy = 2.0 / (f32)fb->height;
+        f32 pfx = -1.0;
+        f32 pfy = -1.0;
         for(u64 y = 0; y < fb->height; y++)
-        for(u64 x = 0; x < fb->width; x++)
         {
-            u64 i = x + y * fb->width;
-
-            f32 pfx = ((f32)x / (f32)fb->width) * 2.0 - 1.0;
-            f32 pfy = ((f32)y / (f32)fb->height) * 2.0 - 1.0;
-
-            f32 s = cosineF32(time * 2*M_PI);
-            f32 c = sineF32(time * 2*M_PI);
-            f32 fx = pfx*c - pfy*s;
-            f32 fy = pfy*c + pfx*s;
-
-            if(fx > -0.5 && fx < 0.5 && fy > -0.5 && fy < 0.5)
+            for(u64 x = 0; x < fb->width; x++)
             {
-                fb->data[i*4 + 0] = red;
-                fb->data[i*4 + 1] = green;
-                fb->data[i*4 + 2] = blue;
-                fb->data[i*4 + 3] = 0.6;
+                u64 i = x + y * fb->width;
+
+                if(
+                (pfx+p1x) * d1y - (pfy+p1y) * d1x < 0.0 &&
+                (pfx+p2x) * d2y - (pfy+p2y) * d2x < 0.0 &&
+                (pfx+p3x) * d3y - (pfy+p3y) * d3x < 0.0 &&
+                (pfx+p4x) * d4y - (pfy+p4y) * d4x < 0.0
+                )
+                {
+                    fb->data[i*4 + 0] = red;
+                    fb->data[i*4 + 1] = green;
+                    fb->data[i*4 + 2] = blue;
+                    fb->data[i*4 + 3] = 0.6;
+                }
+                else
+                {
+                    fb->data[i*4 + 0] = 0.0;
+                    fb->data[i*4 + 1] = 0.0;
+                    fb->data[i*4 + 2] = 0.0;
+                    fb->data[i*4 + 3] = 0.0;
+                }
+                pfx += dpfx;
             }
-            else
-            {
-                fb->data[i*4 + 0] = 0.0;
-                fb->data[i*4 + 1] = 0.0;
-                fb->data[i*4 + 2] = 0.0;
-                fb->data[i*4 + 3] = 0.0;
-            }
+        pfx = -1.0;
+        pfy += dpfy;
         }
         AOS_surface_commit(surface);
+        f64 frame_end = AOS_time_get_seconds();
+        printf("elf time : %10.10lf ms\n", (frame_end - frame_start) * 1000.0);
     }
 }
 }
